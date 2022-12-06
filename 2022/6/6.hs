febca66b9f301b196551133af933d705752d05a5
Jiawen Li <jl8322@aube.doc.ic.ac.uk>
2022-12-06 05:19:56 +0000

    Complete day 6 :)

---
+++ 2022/6/6.hs
@@ -0,0 +1,22 @@
+import Data.List
+
+pLen :: Int
+pLen = 4
+
+mLen :: Int 
+mLen = 14
+
+getMarkerChars :: String -> Int -> Int -> Int 
+getMarkerChars s len n 
+  | length s < len || nub xs' == xs' = n
+  | otherwise                        = getMarkerChars (xs ++ s') len (n + 1)
+  where (xs'@(_ : xs), s') = splitAt len s
+
+main = do 
+  text <- readFile "input6.txt"
+
+  let pChars = getMarkerChars text pLen pLen
+  let mChars = getMarkerChars text mLen mLen
+ 
+  putStrLn $ "Start-of-packet marker is at: " ++ show pChars
+  putStrLn $ "Start-of-message marker is at: " ++ show mChars
